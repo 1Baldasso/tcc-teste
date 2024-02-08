e7caff96df0462e148a82a27aacc7333d1e646e0
Lucas Baldasso <engs-lucasbaldasso@camporeal.edu.br>
2024-02-08 19:22:48 -0300

    fixing keys

--- src/transpiler/type_mapper.rs
+++ src/transpiler/type_mapper.rs
@@ -1,12 +1,9 @@
-use std::borrow::Borrow;
-
 pub trait DataTypeMapper {
-    fn map_data_type(self) -> String;
+    fn map_data_type(self, has_key: bool, default_key_type: &str) -> String;
 }
 
 impl DataTypeMapper for &str {
-    fn map_data_type(self) -> String {
-        let (has_key, default_key_type) = (true, "UNIQUEIDENTIFIER");
+    fn map_data_type(self, has_key: bool, default_key_type: &str) -> String {
         let data_type = self.to_lowercase().replace('"', "");
         let result = match data_type.as_str() {
             "string" => "VARCHAR(MAX)",
--- src/transpiler/visitor.rs
+++ src/transpiler/visitor.rs
@@ -2,6 +2,7 @@ use json::JsonValue;
 use arrow::util::string_writer::StringWriter;
 use std::io::Write;
 use crate::{transpiler::type_mapper::DataTypeMapper, Cli};
+use std::borrow::Borrow;
 
 pub trait NodeVisitor {
     fn visit_node(&mut self, writer: &mut StringWriter, args: &Cli) -> String;
@@ -11,7 +12,7 @@ impl NodeVisitor for JsonValue {
     fn visit_node(&mut self,  writer: &mut StringWriter, args: &Cli) -> String {
         let json_val = self;
         if json_val.is_string() {
-            return json_val.dump().map_data_type();
+            return json_val.dump().map_data_type(!args.no_keys, args.key_type.borrow());
         } else if json_val.is_object() {
             for (key, val) in json_val.entries_mut() {
                 if val.is_object()
@@ -22,7 +23,7 @@ impl NodeVisitor for JsonValue {
                 let result = val.visit_node(writer, args);
                 if !result.is_empty()
                 {
-                    let mut formatted_string = String::new();
+                    let formatted_string: String;
                     if result.to_lowercase() == args.key_type.to_lowercase() {
                         formatted_string = format!("{}Id {} FOREIGN KEY REFERENCES {}(Id)", key, result, key)
                     }
